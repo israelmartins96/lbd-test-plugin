54dba230b3da2b9b6c4099b1f8a920a4f04442f9
Israel Martins <m.oisrael96@gmail.com>
2024-07-07 14:16:30 +0100

    First commit
    Added plugin main PHP file, GPL v2 licence file, and plugin index.php file

---
+++ lbd-test-plugin.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * LbD Test Plugin
+ *
+ * @package             LbDTestPlugin
+ * @author              Israel Martins
+ * @copyright           Copyright (c) 2024, Lightbulb Devs
+ * @license             GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:         LbD Test Plugin
+ * Plugin URI:          https://israelmartins.com
+ * Description:         A test plugin
+ * Version:             0.1.0
+ * Requires at least:   5.2
+ * Requires PHP:        7.2
+ * Author:              Israel Martins
+ * Author URI:          https://israelmartins.com
+ * Donate link:         https://israelmartins.com/donate/
+ * License:             GPL v2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:          https://israelmartins.com
+ * Text Domain:         lbd-test-plugin
+ * Domain Path:         /languages/
+ */
+
+ /*
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ Copyright 2024 Lightbulb Devs
+ */
