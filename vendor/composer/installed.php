0070af3dbfca24d5ff3d453f0cc672966ff02f1d
Israel Martins <m.oisrael96@gmail.com>
2024-07-16 21:09:50 +0100

    Added PHP Composer
    Added Composer to project for Autoload and Namespacing support.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'lbd-test-plugin/lbd-test-plugin',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '20b12057588eaeb5067b2e2596173a8cbf1ceee0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'lbd-test-plugin/lbd-test-plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '20b12057588eaeb5067b2e2596173a8cbf1ceee0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
